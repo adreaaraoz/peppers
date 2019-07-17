92cd03db82da133a1e6b8c4a0083921db2222c38
Adrea Araoz <adreaaraoz@Adreas-MacBook-Pro.local>
2019-07-16 20:19:34 -0400

    adding mobiel products

---
+++ script.js
@@ -0,0 +1,40 @@
+var redShades = {
+  title: 'Red',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-red.jpg',
+  price: 99
+};
+var greenShades = {
+  title: 'Green',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-green.jpg',
+  price: 99
+};
+var blueShades = {
+  title: 'Blue',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-blue.jpg',
+  price: 99
+};
+var blackShades = {
+  title: 'Black',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-black.jpg',
+  price: 99
+};
+var shades[ redShades, greenShades, blueShades, blackShades ] ;
+for (var i=0; i < shades.lenght; i++) { console.log (shades)
+
+  <div class="col peppers-product text-center">
+         <img class="img-fluid" src=" ${ product.img }" alt="Peppers Rock-N-Stroll Red">
+         <p class="product-title">[SUBTITLE]</p>
+         <h3>{ $ product.img }</h3>
+         <p class="product-price">$[PRICE]</p>
+         <div class="product-cta-container">
+           <a href="#" class="btn btn-danger btn-lg">Add to Cart</a>
+         </div>
+       </div>
+
+       $(document).ready(function() {
+  $('#mobile-products').html(mobileProducts( shades[0] ));
+});
